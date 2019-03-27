846539b7f718afb7ff044c33a80d1f4dc60ada61
xuguozhi <xuguozhi@llewan.com>
2019-03-27 15:53:34 +0800

    优先完成zookeeper部分

---
+++ docker.install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#docker安装
+wget -qO- https://get.docker.com/ | sh
---
+++ zookeeper.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#zookeeper启动
+
+dir=${PWD}
+
+# 创建容器hosts
+: > ${PWD}/zookeeper/etc/hosts
+myid=0
+servers=""
+length=`cat ${PWD}/zookeeper/instances.yml | shyaml get-length instances`
+thisServerIpString=(`cat ${PWD}/zookeeper/server.yml | shyaml get-value server.0.ip`)
+thisServerIp=${thisServerIpString[1]}
+for((i=0;i<${length};i++));
+do
+  ipString=(`cat ${PWD}/zookeeper/instances.yml | shyaml get-value instances.${i}.ip`)
+  dnsString=(`cat ${PWD}/zookeeper/instances.yml | shyaml get-value instances.${i}.dns`)
+  idString=(`cat ${PWD}/zookeeper/instances.yml | shyaml get-value instances.${i}.id`)
+  ip=${ipString[1]}
+  dns=${dnsString[1]}
+  id=${idString[1]}
+  echo "${ip}   ${dns}" >> ${PWD}/zookeeper/etc/hosts
+  servers="${servers}server.${id}=${dns}:2888:3888 "
+  # 判断zookeeper服务id
+  if [ "${thisServerIp}" == "${ip}" ] ;then
+    myid=${id}
+  fi
+done
+
+#############################启动zookeeper
+# 如果使用环境变量来定义相关配置则不应挂载zoo.cfg脚本，系统会通过环境变量生成zoo.cfg配置文件
+docker pull zookeeper:3.4.13
+docker rm $(docker ps -a| grep "zookeeper" |cut -d " " -f 1) -f
+docker run -d --name zookeeper --net=host \
+    -v ${PWD}/zookeeper/etc/hosts:/etc/hosts \
+    -v ${PWD}/zookeeper/datalog:/datalog \
+    -v ${PWD}/zookeeper/logs:/logs \
+    -v ${PWD}/zookeeper/data:/data \
+    -e ZOO_MY_ID=${myid} \
+    -e ZOO_SERVERS="${servers}" \
+    zookeeper:3.4.13
