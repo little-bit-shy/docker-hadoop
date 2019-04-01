138a5e3137e33e220054b5031c36fd290f83713f
xuguozhi <xuguozhi@llewan.com>
2019-04-01 13:51:04 +0800

    优化hadoop搭建过程

--- hadoop.sh
+++ hadoop.sh
@@ -5,6 +5,7 @@ dir=/usr/local/hadoop
 
 # 修改项目权限
 chown 1000:1000 -R ${PWD}/hadoop
+chmod 644 ${PWD}/hadoop/known_hosts
 
 # 创建容器hosts
 : > ${dir}/hadoop/etc/hosts
@@ -24,12 +25,13 @@ done
 docker build --network host -t hadoop ${dir}/hadoop
 docker rm $(docker ps -a| grep "hadoop" |cut -d " " -f 1) -f
 docker run -d --name hadoop --net=host  \
+    -v ${dir}/hadoop/known_hosts:/home/hadoop/.ssh/known_hosts \
     -v ${dir}/hadoop/etc/hosts:/etc/hosts \
-     -v ${dir}/hadoop/etc/hadoop:/usr/local/hadoop/etc/hadoop \
-     -v ${dir}/hadoop/dfs:/usr/local/hadoop/dfs \
-     -v ${dir}/hadoop/jn:/usr/local/hadoop/jn \
-     -v ${dir}/hadoop/tmp:/usr/local/hadoop/tmp \
-     -v ${dir}/hadoop/logs:/usr/local/hadoop/logs \
-     -v ${dir}/hive/conf/hive-site.xml:/usr/local/hive/conf/hive-site.xml \
-     -v ${dir}/hive/mysql-connector-java-5.1.47/mysql-connector-java-5.1.47.jar:/usr/local/hive/lib/mysql-connector-java-5.1.47.jar \
+    -v ${dir}/hadoop/etc/hadoop:/usr/local/hadoop/etc/hadoop \
+    -v ${dir}/hadoop/dfs:/usr/local/hadoop/dfs \
+    -v ${dir}/hadoop/jn:/usr/local/hadoop/jn \
+    -v ${dir}/hadoop/tmp:/usr/local/hadoop/tmp \
+    -v ${dir}/hadoop/logs:/usr/local/hadoop/logs \
+    -v ${dir}/hive/conf/hive-site.xml:/usr/local/hive/conf/hive-site.xml \
+    -v ${dir}/hive/mysql-connector-java-5.1.47/mysql-connector-java-5.1.47.jar:/usr/local/hive/lib/mysql-connector-java-5.1.47.jar \
     hadoop
